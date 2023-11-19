589d3749b2dcd8fb7654f453f40ebe3c1015bfaf
Melody Duncan <melody.d.duncan@gmail.com>
2023-11-19 16:02:16 -0500

    Ensure Mantine is functional

---
+++ src/components/ProjectList.tsx
@@ -0,0 +1,11 @@
+import { Alert } from '@mantine/core';
+
+function ProjectList() {
+    return (
+        <Alert variant="light" color="violet" radius="lg" title="Hello">
+            This is the welcome message to test all is well
+        </Alert>
+    )
+}
+
+export default ProjectList;
