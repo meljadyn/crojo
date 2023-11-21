7ef13ef578246d0f938489a13af3c64f0a482fce
Melody Duncan <melody.d.duncan@gmail.com>
2023-11-21 13:55:51 -0500

    Remove prisma and mantine, rename project

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,9 @@
-import { MantineProvider } from "@mantine/core"
-import '@mantine/core/styles.css';
 import ProjectList from "./components/ProjectList.tsx";
 import './App.css'
 
 function App() {
   return (
-    <MantineProvider>
-        <ProjectList />
-    </MantineProvider>
+      <ProjectList />
   )
 }
 
--- src/components/ProjectList.tsx
+++ src/components/ProjectList.tsx
@@ -1,10 +1,7 @@
-import { Alert } from '@mantine/core';
 
 function ProjectList() {
     return (
-        <Alert variant="light" color="violet" radius="lg" title="Hello">
-            This is the welcome message to test all is well
-        </Alert>
+        <p>This is the welcome message to test all is well</p>
     )
 }
 
